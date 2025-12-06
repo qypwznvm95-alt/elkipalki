71ceb2222c4a74002623a91d16a219555223f793
Denisenko <qypwznvm95@privaterelay.appleid.com>
2025-12-06 15:32:13 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+# Конфигурация бота
+TOKEN = "8010531035:AAESNOu4k-btr0tYEoV11YSGCNULqGIKoco"
+ADMIN_CHAT_ID = "@rose_azov"  # Ваш рабочий канал
+ADMIN_USER_ID = 134015771  # Ваш личный ID для уведомлений
+
+# Цены
+PRICES = {
+    "S": 1000,
+    "M": 2000,
+    "L": 3000
+}
+DELIVERY_COST = 500
+MIN_BUDGET = 3000
+MAX_BUDGET = 100000
+
+# Контакты
+CONTACTS = {
+    "whatsapp": "https://wa.me/79188999004",
+    "telegram": "@rose_azov"
+}
